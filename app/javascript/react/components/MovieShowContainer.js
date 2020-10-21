d55bdbd5ac723696dcc2b4f3fb05d5a6916ed9cc
Chris Rackey <chrisrackey@MacBook-Pro.local>
2020-10-21 15:05:21 -0400

    added a show page

---
+++ app/javascript/react/components/MovieShowContainer.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react"
+
+import MovieShow from "./MovieShow"
+
+const MovieShowContainer = (props) => {
+    const [movie, setMovie] = useState({}) 
+    const id = props.match.params.id 
+    useEffect(() => {
+        fetch(`/api/v1/movies/${id}`)
+        .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            let errorMessage = `${response.status} (${response.statusText})`,
+                error = new Error(errorMessage);
+            throw(error);
+        }
+        })
+        .then((responseBody) => {
+            debugger
+            setMovie(responseBody)
+        })
+        .catch((error) => console.error(`Error in fetch: ${error.message}`))
+    }, [])
+    return (
+        <MovieShow 
+            id={movie.id}
+            title={movie.title}
+            summary={movie.summary}
+            year={movie.year}
+        />
+    )
+}
+
+export default MovieShowContainer 
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
 
   get "/movies", to: "homes#index"
 
+  get "/movies/:id", to: "homes#index"
+
+
   namespace :api do
     namespace :v1 do
-      resources :movies, only: [:index]
+      resources :movies, only: [:index, :show]
     end
   end
     
