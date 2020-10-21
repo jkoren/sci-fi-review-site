98d961711c0db09fb542c6f1df4075b1e34c8c7c
Chris Rackey <chrisrackey@MacBook-Pro.local>
2020-10-21 16:33:00 -0400

    removed the items suggested, and fixed the formatting to tabs of size 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :homes, only: [:index]
   
   root 'homes#index'
   get "/movies", to: "homes#index"
@@ -14,3 +13,4 @@ Rails.application.routes.draw do
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
