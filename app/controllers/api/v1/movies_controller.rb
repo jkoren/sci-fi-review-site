feccedbeb2dc4ccd8d7c7087565951b704e082fe
liishi <liishidurbin@gmail.com>
2020-10-26 10:12:21 -0400

    staged changes

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -11,8 +11,12 @@ class Api::V1::MoviesController < ApplicationController
   end
 
   def create
-    new_movie = Movie.new(movie_params)
-    binding.pry
+    new_movie = Movie.new
+    new_movie.title = movie_params["title"]
+    new_movie.summary = movie_params["summary"]
+    new_movie.year = movie_params["year"]
+    new_movie.movie_poster = movie_params["movie_poster"]
+
     if new_movie.save
       render json: new_movie
     else
