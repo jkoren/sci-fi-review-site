678084130206cdcf04a3f799bbca2c8005b69e9e
liishi <liishidurbin@gmail.com>
2020-10-29 10:26:45 -0400

    finished voting feature with styling

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ReviewsController < ApplicationController
 
   def create
+    binding.pry
     movie = Movie.find(params[:movie_id])
     new_review = Review.new(review_params)
     new_review.movie = movie
@@ -15,4 +16,4 @@ class Api::V1::ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :body)
   end
-  
+end
--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react"
 
 const ReviewTile = (props) => {
-  const [upvotes, setUpvotes] = useState()
+  const [votes, setVotes] = useState()
   const [error, setError] = useState("")
-
   useEffect(() => {
     setError("")
     let votePayLoad = {
@@ -29,22 +28,13 @@ const ReviewTile = (props) => {
     })
     .then(response => response.json())
     .then(body => {
-      setUpvotes(body) 
+      setVotes(body) 
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }, [])
-
   const voteHandler = (reviewID, vote, event) => {
     setError("")
-
     event.preventDefault()
-    // let userVote = new FormData()
-    // userVote.append("vote[type]", vote)
-    // fetch(`/api/v1/reviews/${reviewID}/votes.json`, {
-    //   method: "POST",
-    //   credentials: "same-origin",
-    //   body: userVote
-    // })
     let votePayLoad = {
       reviewID: reviewID,
       voteType: vote
@@ -66,27 +56,40 @@ const ReviewTile = (props) => {
           error = new Error(errorMessage)
         throw error
       }
-    })
+    }) 
     .then(response => response.json()) 
     .then(body => {
         if (isNaN(body)) {
           setError(body)
         } else {
-          setUpvotes(body)
+          setVotes(body)
         }
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }
-  
   return(
     <div className="callout secondary cell small-6">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"/>
       <p>{props.rating} - {props.body}</p>
       <p>{error}</p>
-      <button className="button" onClick={(event) => voteHandler(props.id, 1, event)}>Upvote</button>
-      <p>{upvotes}</p>
-      <button className="button" onClick={(event) => voteHandler(props.id, -1, event)}>Downvote</button>
+      <span>
+        <button className="button" onClick={(event) => voteHandler(props.id, 1, event)}>Upvote</button>
+      </span>
+      <span>
+        <button className="button" onClick={(event) => voteHandler(props.id, -1, event)}>Downvote</button>
+      </span>
+      <span>
+        {votes == 1
+          ?  <i className="fas fa-arrow-circle-up"></i>
+          : <i className="fas fa-arrow-up"></i>
+        }
+        {votes == -1
+          ? <i className="fas fa-arrow-circle-down"></i>
+          : <i className="fas fa-arrow-down"></i>
+        }
+        {votes}
+      </span>
     </div>
   )
 }
-
 export default ReviewTile
